94f5010b30e68f5b5e081bdeb4a32101f0b6ff91
Fazilat Jahan <fazilat.jahan07@gmail.com>
2024-12-05 18:29:13 +0500

    Figma Clone Assignment

--- figma-design-clone/src/app/components/HeroSection.tsx
+++ figma-design-clone/src/app/components/HeroSection.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const HeroSection = () => {
     return (
         <div className=' flex w-[1920px] h-[829px] m-auto px-[220px] py-[140px] bg-[#043873]'>
-            <div className='w-1/2 flex  '>
-                <div className='my-[60px] w-[656px] h-[361px] font-[inter] text-white '>
+            <div className='w-1/2 flex items-center justify-center '>
+                <div className='mb-[60px] w-[656px] h-[361px] font-[inter] text-white '>
 
                     <h1 className=' text-[64px] leading-[auto] font-bold my-6'>Get More Done With whitepace</h1>
                     <h2 className=' text-[18px] font-light leading-[30px]'>Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks</h2>
@@ -12,8 +12,8 @@ const HeroSection = () => {
                 </div>
             </div>
 
-            <div className='w-1/2 flex'>
-                <div className='bg-[#C4DEFD] w-[824px] h-[549px] m-[10px] '></div>
+            <div className='w-1/2 flex items-center justify-center'>
+                <div className='bg-[#C4DEFD] w-[824px] h-[549px] m-[4px] '></div>
             </div>
 
         </div>
